a8816258f2168dc09dd37133beae87e96376c4b5
LordNem <lordn3mesis@hotmail.com>
2017-02-23 18:03:54 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,10 +5,8 @@ tar xf libesedb-20160622.tar.gz
 cd libesedb-20160622
 ./configure
 make
-cd $CWD resources/
-cd ntdsxtract2/
-chmod +x *.py
 
+printf "\033c"
 echo "Configuration has completed"
 echo "run adXtract.sh /pathto/ntds.dit /pathto/SYSTEM /ProjectName"
 
