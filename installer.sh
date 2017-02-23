d768c6a3876896894104efd8707f51f2e3dfeba7
LordNem <lordn3mesis@hotmail.com>
2017-02-23 18:52:19 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,9 +5,7 @@ tar xf libesedb-20160622.tar.gz
 cd libesedb-20160622
 ./configure
 make
-
 printf "\033c"
 echo "Configuration has completed"
 echo "run adXtract.sh /pathto/ntds.dit /pathto/SYSTEM /ProjectName"
-
 
