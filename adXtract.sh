b68b93fafd6125658e3961cd652f7db101b960b2
LordNem <lordn3mesis@hotmail.com>
2017-02-23 18:51:54 +0000

    Update adXtract.sh

--- adXtract.sh
+++ adXtract.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
-
-
 printf "\33c"
 
 if [ -z $2 ]; then
@@ -11,24 +8,17 @@ if [ -z $2 ]; then
         exit 1;
 
 fi
-
 CWD="$(pwd)"
 echo "the working dir is " $CWD
 mkdir '/root/Desktop/adXtract'.$3
 mkdir '/root/Desktop/adXtract/Maps'
-
 cd $CDW resources/libesedb-20160622/esedbtools
-
 echo "This could take a while... time for coffee!"
-
-./esedbexport -t /root/Desktop/adXtract/$3 $1 > /root/Desktop/adXtract/exported
-
+./esedbexport -t /root/Desktop/adXtract.$3/$3 $1 > /root/Desktop/adXtract/exported
 cd ../../
-
-cd $CDW ntdsxtract/
-
-./dsusers.py /root/Desktop/adXtract/$3.export/datatable.3 /root/Desktop/adXtract/$3.export/link_table.5 /root/Desktop/adXtract/Maps/ --passwordhashes --pwdformat ophc --syshive $2 --lmoutfile /root/Desktop/adXtract/allLMhashes.txt --ntoutfile /root/Desktop/adXtract/allNTLMhashes.txt
-
-cat /root/Desktop/adXtract/allNTLMhashes.txt
-echo "Check /root/Desktop/adXtract/allLMhashes.txt"
-
+echo "the working dir is " $CWD
+cd $CWD 
+cd resources/ntdsxtract2/
+./dsusers.py /root/Desktop/adXtract.$3/$3.export/datatable.3 /root/Desktop/adXtract.$3/$3.export/link_table.5 /root/Desktop/adXtract/Maps/ --passwordhashes --pwdformat ophc --syshive $2 --lmoutfile /root/Desktop/adXtract.$3/$3.allLMhashes.txt --ntoutfile /root/Desktop/adXtract.$3/$3.allNTLMhashes.txt
+cat /root/Desktop/adXtract.$3/$3.allNTLMhashes.txt
+echo "Check /root/Desktop/adXtract."$3"/"$3"allLMhashes.txt"
