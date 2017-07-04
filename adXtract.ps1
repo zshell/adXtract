81b68caa65e093f860689a95a755e44ad8621eeb
LordNem <lordn3mesis@hotmail.com>
2017-07-04 22:37:58 +0100

    Update adXtract.ps1

--- adXtract.ps1
+++ adXtract.ps1
@@ -1,17 +1,19 @@
 cls
 
-
-Write-host "            ___  ___                  _   "
-Write-host "   __ _  __| \ \/ / |_ _ __ __ _  ___| |_ "
-Write-host "  / _` |/ _` |\  /| __| '__/ _` |/ __| __|"
-Write-host " | (_| | (_| |/  \| |_| | | (_| | (__| |_ "
-Write-host "  \__,_|\__,_/_/\_\\__|_|  \__,_|\___|\__|"
-Write-host "                                          "
+Write-Host @"
+            ___  ___                  _   
+   __ _  __| \ \/ / |_ _ __ __ _  ___| |_ 
+  / _` |/ _` |\  /| __| '__/ _` |/ __| __|
+ | (_| | (_| |/  \| |_| | | (_| | (__| |_ 
+  \__,_|\__,_/_/\_\\__|_|  \__,_|\___|\__|
+                                          
+"@ -fo green
 
 $cwd = (Resolve-Path .\).Path
+Write-Host $cwd -fo red
 
 #Setting up the Config File for first time run:
-Write-Host "Setting up Config file"
+Write-Host "Setting up Config file" -fo yellow
 If(Test-Path .\config.txt) {
 $HCPath = get-content ".\config.txt" | select-string -pattern hashcat
 $Esedb =  get-content ".\config.txt" | select-string -pattern win_libesedb 
@@ -36,22 +38,21 @@ If(Test-Path .\config.txt) { } Else {
 }
 
 #Extracting the NTDS.dit:
-Write-Host "Config Set"
+Write-Host "Config Set" -fo yellow
 $DBpath = Read-Host "Enter path to the ntds.dit and SYSTEM files"
 
 #Extracting the table information:
-Write-host "Extracting table information"
-Write-host "############################"
-Write-host "########***********#########"
-Write-host "########***********#########"
-Write-host "########***********#########"
-Write-host "############################"
+Write-host "Extracting table information" -fo Yellow
+Write-Host @"
+############################
+"@ -fo green
+
 cmd /c "$Esedb\esedbexport.exe" "$DBPath\ntds.dit"
 mv ntds.dit.export $DBPath
 
 #Extracting User hashes and historic hashes into Hashcat format:
-Write-host "Extracting ntds, this can take a while"
-cmd /c "$python\python.exe" "$cwd\adXtract\resources\ntdsxtract2\dsusers.py" "$DBPath\ntds.dit.export\datatable.3" "$DBPath\ntds.dit.export\link_table.5" "$DBpath\ntds.dit.export" --passwordhashes --pwdformat ophc --syshive "$DBpath\SYSTEM" --passwordhistory --ntoutfile "$DBPath\hashes-humanreadable.hash"
+Write-host "Extracting ntds, this can take a while" -fo red
+cmd /c "$python\python.exe" "$cwd\resources\ntdsxtract2\dsusers_colour.py" "$DBPath\ntds.dit.export\datatable.3" "$DBPath\ntds.dit.export\link_table.5" "$DBpath\ntds.dit.export" --passwordhashes --pwdformat ophc --syshive "$DBpath\SYSTEM" --passwordhistory --ntoutfile "$DBPath\hashes-humanreadable.hash"
 
 #Crackng passwords using hashcat: 
 cmd /c "$HCPath\hashcat64.exe" -m 1000 "$DBPath\hashes-humanreadable.hash" "$words\*.txt" -w3 --rules "$rules\1_top_500.rule"
